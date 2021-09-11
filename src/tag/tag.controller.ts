439b485ddb6e65f764e5411eed12a842961f97f5
Vladimir <fayst_@bk.ru>
2021-09-12 02:22:09 +0300

    Реализует загрузку списка tags из базы данных.

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+
 import { TagService } from '@app/tag/tag.service';
 
 @Controller('tags')
 export class TagController {
   constructor(private readonly tagService: TagService) {}
   @Get()
-  findAll(): string[] {
-    return this.tagService.getTags();
+  async findAll(): Promise<{ tags: string[] }> {
+    const tags = await this.tagService.getTags();
+    return {
+      tags: tags.map((tag) => tag.name),
+    };
   }
 }
--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { TagEntity } from '@app/tag/tag.entity';
 
 @Injectable()
 export class TagService {
-  getTags = (): string[] => {
-    return ['Angular', 'React', 'Vue', 'Svetle'];
+  constructor(
+    @InjectRepository(TagEntity)
+    private readonly tagRepository: Repository<TagEntity>,
+  ) {}
+
+  getTags = async (): Promise<TagEntity[]> => {
+    return await this.tagRepository.find();
   };
 }
